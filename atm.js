bc645a0af75d1b0ba49054418b6be72e813402c9
Frew A Negash <zulufanke@yahoo.com>
2017-07-10 12:32:11 -0400

    checked buttons

--- atm.js
+++ atm.js
@@ -1,3 +1,24 @@
+/* global $ */
 $(document).ready(function(){
+
 // Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
-});
+$('body').css('background', 'red')
+
+// testing jquery url
+$('body').css('background', 'red')
+//declare variable
+var bDep = $('#checking .deposit')
+var bWith = $('#checking .withdraw')
+
+var activity = function (){
+  console.log('Hello Customr!')
+  $('#checking .balance').text($('#checking .input') .val())
+}
+
+bDep.on('click', activity)
+bWith.on('click', activity)
+
+var bDep= $('#checking .deposit')
+var bWith = $('#checking .withdraw')
+
+  })
